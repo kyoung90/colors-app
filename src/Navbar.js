90af64457a589421a641e64a8826e0f36dfae5aa
kyoung90 <kenneth.young90@gmail.com>
2019-11-19 17:39:05 -0400

    Add select and material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
-export const Navbar = ({ level, changeLevel }) => {
+export const Navbar = ({ level, changeLevel, handleSelect }) => {
+  const [format, setFormat] = useState("hex");
+
+  const handleChange = event => {
+    setFormat(event.target.value);
+    handleSelect(event.target.value);
+  };
   return (
     <header className="Navbar">
       <div className="logo">
@@ -20,6 +28,13 @@ export const Navbar = ({ level, changeLevel }) => {
             onAfterChange={changeLevel}
           />
         </div>
+      </div>
+      <div className="select-container">
+        <Select value={format} onChange={handleChange}>
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255, 210, 10)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255, 210, 10, 1.0)</MenuItem>
+        </Select>
       </div>
     </header>
   );
--- src/Palette.js
+++ src/Palette.js
@@ -7,18 +7,27 @@ export const Palette = props => {
   const { colors } = props.palette;
 
   const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState("hex");
 
   const colorBoxes = colors[level].map(color => (
-    <ColorBox background={color.hex} name={color.name} />
+    <ColorBox background={color[format]} name={color.name} />
   ));
 
   const changeLevel = lvl => {
     setLevel(lvl);
   };
 
+  const changeFormat = val => {
+    setFormat(val);
+  };
+
   return (
     <div className="Palette">
-      <Navbar level={level} changeLevel={changeLevel} />
+      <Navbar
+        level={level}
+        changeLevel={changeLevel}
+        handleSelect={changeFormat}
+      />
       <div className="Palette-colors">{colorBoxes}</div>
       {/*Footer eventually */}
     </div>

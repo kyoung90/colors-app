0d9f21077ca3a0e87ac8cc52ecde1ba6524db4a5
kyoung90 <kenneth.young90@gmail.com>
2019-11-20 14:05:59 -0400

    Add snackbar and styled select

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import Slider from "rc-slider";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
+import IconButton from "@material-ui/core/IconButton";
 
 export const Navbar = ({ level, changeLevel, handleSelect }) => {
   const [format, setFormat] = useState("hex");
+  const [open, setOpen] = useState(false);
 
-  const handleChange = event => {
+  const handleFormatChange = event => {
+    setOpen(true);
     setFormat(event.target.value);
     handleSelect(event.target.value);
+  };
+
+  const closeSnackbar = () => {
+    setOpen(false);
   };
   return (
     <header className="Navbar">
@@ -30,12 +39,31 @@ export const Navbar = ({ level, changeLevel, handleSelect }) => {
         </div>
       </div>
       <div className="select-container">
-        <Select value={format} onChange={handleChange}>
+        <Select value={format} onChange={handleFormatChange}>
           <MenuItem value="hex">HEX - #ffffff</MenuItem>
           <MenuItem value="rgb">RGB - rgb(255, 210, 10)</MenuItem>
           <MenuItem value="rgba">RGBA - rgba(255, 210, 10, 1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={open}
+        autoHideDuration={3000}
+        message={
+          <span id="message-id">Format Changed To {format.toUpperCase()}!</span>
+        }
+        ContentProps={{ "aria-describedby": "message-id" }}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon></CloseIcon>
+          </IconButton>
+        ]}
+      />
     </header>
   );
 };
